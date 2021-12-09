1b608b919840b02482355a1a6124d6ea4f89b701
Protix <azadseker@hotmail.com>
2021-12-09 10:46:48 +0100

    Created some new classes to create the ship.

--- game/game.js
+++ game/game.js
@@ -1,8 +1,31 @@
+var ship;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
 }
   
 function draw() {
-  background(60);
-  circle(210,98,50)
+  background(0);
+  ship.render();
+}
+
+function Ship() {
+  this.pos = createVector(width/2, height/2);
+  this.r = 20;
+  this.heading = 0;
+
+
+
+  this.render = function() {
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading);
+    noFill();
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
+  }
+
+  this.turn = function(angle) {
+    heading += angle;
+  }
 }
