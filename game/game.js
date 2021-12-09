6602e5605d12b1f2c74e224545f79c40b13f17f0
Protix <azadseker@hotmail.com>
2021-12-09 10:23:06 +0100

    test

---
+++ game/game.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(400, 400);
+}
+  
+function draw() {
+  background(60);
+  circle(210,98,50)
+}
--- game/js/game.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(300);
-    circle(210,98,10)
-  }
