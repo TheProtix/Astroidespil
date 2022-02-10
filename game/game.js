75e3868e90225a8a6b54b19880ecbc467618ecd3
Protix <azadseker@hotmail.com>
2022-02-10 08:10:40 +0100

    t

--- game/game.js
+++ game/game.js
@@ -1,31 +1,37 @@
 var ship;
+var asteroids = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
+  for (var i = 0; i < 5; i++) {
+    asteroids.push(new Asteroid());
+  }
 }
   
 function draw() {
   background(0);
   ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+
+  for(var i = 0; i < asteroids.length; i++) {
+    asteroids[i].render();
+  }
 }
 
-function Ship() {
-  this.pos = createVector(width/2, height/2);
-  this.r = 20;
-  this.heading = 0;
+function keyReleased(){
+  ship.setRotation(0);
+  ship.boosting(false);
+}
 
-
-
-  this.render = function() {
-    translate(this.pos.x, this.pos.y);
-    rotate(this.heading);
-    noFill();
-    stroke(255);
-    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
-  }
-
-  this.turn = function(angle) {
-    heading += angle;
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if(keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if(keyCode == UP_ARROW) {
+    ship.boosting(true);
   }
 }
